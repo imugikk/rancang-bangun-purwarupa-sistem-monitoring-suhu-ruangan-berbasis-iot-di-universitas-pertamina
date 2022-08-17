5d63d78635c00cbaddf47ae307181a8dd82d0006
imugikk <ugyparaptughessa@gmail.com>
2022-08-17 22:10:04 +0700

    Update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class UserController extends Controller
 {
@@ -36,6 +37,7 @@ class UserController extends Controller
         $validated['role_id'] = (int) $request->role;
 
         User::create($validated);
+        Alert::success('Congrats', 'You\'ve Successfully Added an User');
 
         return redirect(url('users'));
     }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,6 +21,7 @@
 <body class="nav-fixed">
     @include('layouts.navbar')
     @include('layouts.sidebar')
+    @include('sweetalert::alert')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
